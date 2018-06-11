669e90c2d718665fcd2431702eebedeba19b4ea6
HiMoxY <36484725+HiMoxY@users.noreply.github.com>
2018-06-11 05:56:07 +0000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,35 @@
+const Eris = require("eris");
+var bot = new Eris("TOKEN");
+var id = "455552914686083073";
+//** لاتعدل على شي تحت ذا **//
+bot.on("ready", () => {
+onstart();
+});
+
+function onstart(){
+
+setInterval(function(){
+
+var currentTime = new Date(),
+hours = currentTime.getHours() + 0 ,
+minutes = currentTime.getMinutes(),
+seconds = currentTime.getSeconds();
+
+if (minutes < 10) {
+minutes = "0" + minutes;
+}
+var suffix = "AM";
+if (hours >= 12) {
+suffix = "PM";
+hours = hours - 12;
+}
+if (hours == 0) {
+hours = 12;
+}
+
+bot.editChannel(id, { name : "●⌠ " + "TIME : " + hours + ":" + minutes + " " + suffix + " ⌡●"});
+}, 60000);
+
+}
+
+bot.connect();
